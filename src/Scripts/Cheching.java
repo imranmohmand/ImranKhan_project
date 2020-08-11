286edacc94f70b157553689a3158e92c3f304af4
imran <imranmohmand@yahoo.com>
2020-08-11 01:14:39 -0400

    just checking

--- src/Scripts/Cheching.java
+++ src/Scripts/Cheching.java
@@ -2,28 +2,22 @@ package Scripts;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FirstApplicationCode {
+public class Cheching {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver(); // Initialize the web_driver to interact wit the Chrome web page
 		driver.manage().window().maximize();//Maximize the window 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);//implicit wait
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);// wait for page full loaded 
 		
-		driver.get("https://www.bing.com/");
-		driver.findElement(By.id("sb_form_q")).sendKeys("selenium webdriver");
-		// actual path has space "search icon tooltip" so i used cssSelector and to cover space we used dot at the
-		//Begging as well as in the space
-		driver.findElement(By.cssSelector(".search.icon.tooltip")).click();
 		
+		driver.get("https://www.facebook.com/");
 		
 		
 		
